377c8eab40be083525880c76ab77142893b75934
s2zan <209rrsz@gmail.com>
2019-11-13 00:21:20 +0900

    index 화면에 등록된 레스토랑 목록 표시
    * 추후 review 테이블과 join하여 랭킹으로 표시하도록 수정해야함

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var connection = require("../lib/db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  connection.query("SELECT * FROM restaurants", function(err, rows) {
+    if (err) {
+      next(err);
+    } else {
+      // console.log("rows", rows);
+      res.render('index', {
+        title: 'Ewha-eats',
+        messages: {
+          success: true
+        },
+        restaurants: rows
+      });
+    }
+  });
 });
 
 module.exports = router;
