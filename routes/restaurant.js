2ca00c8c6c7f7d6cbb0d4e45f4b43508816d79ba
s2zan <209rrsz@gmail.com>
2019-11-13 22:35:55 +0900

    레스토랑 상세 정보 화면 구현

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 var connection = require("../lib/db");
 
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
   res.render('addRestaurant', { title: 'Add New Restaurant' });
 });
 
-router.post('/add', function(req, res, next) {
+router.post('/add', function (req, res, next) {
   try {
     var data = {
       name: req.body.name,
       address: req.body.address
     };
 
-    connection.query("INSERT INTO restaurants SET ?", data, function(err, result) {
+    connection.query("INSERT INTO restaurants SET ?", data, function (err, result) {
       if (err) {
         next(err);
       }
@@ -25,4 +25,43 @@ router.post('/add', function(req, res, next) {
   }
 });
 
+router.get('/:id', function (req, res, next) {
+  connection.query("SELECT * FROM restaurants WHERE id = " + req.params.id, function (
+    err,
+    restaurant,
+    fields
+  ) {
+    if (err) {
+      next(err);
+    }
+    if (restaurant.length <= 0) {
+      res.redirect("/");
+    } else {
+      connection.query("SELECT tags.title "
+                      +"FROM tags "
+                      +"INNER JOIN mapping_tag_restaurant AS m "
+                      +"ON tags.id = m.tag_id "
+                      +"WHERE m.restaurant_id = " + req.params.id, function (
+        err,
+        tags,
+        fields
+      ) {
+        if (err) {
+          next(err);
+        }
+        else {
+          console.log("restaurant", restaurant);
+          res.render("restaurantDetail", {
+            title: "Restaurant Detail",
+            //data: result[0],
+            id: restaurant[0].ID,
+            name: restaurant[0].NAME,
+            address: restaurant[0].ADDRESS,
+            tags: tags
+          });
+        }
+      });
+    }
+  });
+});
 module.exports = router;
